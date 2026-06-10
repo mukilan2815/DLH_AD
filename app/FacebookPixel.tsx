f3b3ab3d247e3d8fb26461ad14bbf9b0e31cddf3
mukilan2815 <mukilan2808@gmail.com>
2026-06-10 11:38:27 +0530

    e

--- app/FacebookPixel.tsx
+++ app/FacebookPixel.tsx
@@ -7,7 +7,7 @@ export default function FacebookPixel() {
     <>
       <Script
         id="facebook-pixel"
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             !function(f,b,e,v,n,t,s)
